6c4b868968975d552b7d5d2664444ec5345ea39e
Eric Wittmann <eric.wittmann@gmail.com>
2014-03-13 13:19:10 -0400

    clean out to prep for website

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo ""
-echo "##############################################"
-echo "  Releasing Design Time Governance (Community)"
-echo "##############################################"
-echo ""
-read -p "Release Version: " RELEASE_VERSION
-read -p "New Development Version: " DEV_VERSION
-mvn -e --batch-mode -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEV_VERSION -DignoreSnapshots=true clean release:prepare release:perform
